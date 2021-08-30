532e386214080ad1b3f30dcd96cc7518139501b5
Dennis <dlex351279@gmail.com>
2021-08-30 20:14:02 +0800

    new @base

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,4 +17,12 @@ module.exports = {
   plugins: [
     require('daisyui'),
   ],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
 }
