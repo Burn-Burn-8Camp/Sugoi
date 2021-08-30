a2babd50f8ed08cfc8a627f5c6150a2b47fc5892
shibao <dream0184@yahoo.com.tw>
2021-08-30 15:51:01 +0800

    install typography -- tailwind plugin

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
 
   resources :products
+
   devise_for :users
+
+  resources :cart,only:[:show, :destroy] do
+    collection do
+      post :add, path: 'add/:id'
+    end
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
