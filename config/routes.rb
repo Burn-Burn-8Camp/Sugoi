547addc752e89e09e18faaa97896c419f356abc5
shibao <dream0184@yahoo.com.tw>
2021-08-31 14:23:42 +0800

    add cart layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,14 @@
 Rails.application.routes.draw do
-
-
-  
+ 
   resources :products
 
-
-
-
-
-
+  resource :cart, only:[:show, :destroy] do
+    collection do
+      post :add, path:'add/:id'
+    end
+  end
 
   get 'hello_world', to: 'hello_world#index'
-
-
-
-
-
 
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }
 
