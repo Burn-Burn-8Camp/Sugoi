5381b04a3a11f2b7c785ae8f00cdf45c6ccf7f8d
wali <larts393@gmail.com>
2021-08-31 18:03:45 +0800

    欄位名字打錯更改

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_170534) do
+ActiveRecord::Schema.define(version: 2021_08_31_095407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2021_08_30_170534) do
     t.string "name"
     t.decimal "price"
     t.integer "quantity"
-    t.text "describtion"
     t.string "category"
     t.string "material"
     t.string "manufacturing_method"
@@ -27,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_08_30_170534) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
