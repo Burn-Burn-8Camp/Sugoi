b2b77bd3ace8a02e6b26764d7a18521812941ba1
joanna <a3011662@gmail.com>
2021-09-02 18:55:26 +0800

    新增標題抓到搜尋按鈕

---
+++ app/javascript/search/search_log.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".input-search")
+
+    button.addEventListener("click", () => {
+        console.log('被按了')
+    })
+})
+
+
+
+
+
+
+
