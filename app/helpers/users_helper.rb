4253b6b4d0b991f55dacf5a098d00588453c5518
jeter <fl526128@gmail.com>
2021-09-02 15:59:57 +0800

    會員分級完成

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,48 @@
 module UsersHelper
-  def rank_icon(rank_name)
-    if rank_name == "general"
-        # content_tag()
-      "<i class='far fa-address-book's></i>" 
-    else
-      "other"
+  def now_rank_icon(accumulated_amount)
+    if accumulated_amount <= 2000
+        # content_tag()  
+      "<i class='far fas fa-award text-6xl text-red-600'></i>"
+    elsif accumulated_amount <= 10000
+      "<i class='fas fa-medal'></i>"
+    elsif accumulated_amount >= 20000
+      "<i class='fas fa-trophy'></i>" 
     end      
+  end
+
+  def up_rank_icon(accumulated_amount)
+    if accumulated_amount <= 2000
+      "<i class='fas fa-medal'></i>"
+    elsif accumulated_amount <= 10000
+      "<i class='fas fa-trophy'></i>"
+    end 
+  end
+
+  def user_rank(accumulated_amount)
+    if accumulated_amount <= 2000
+      "白銀會員"
+    elsif accumulated_amount <= 10000
+      "尊貴會員"
+    # elsif accumulated_amount >= 20000
+     
+    end
+  end
+
+  def up_user_rake(accumulated_amount)
+    if accumulated_amount <= 2000
+      "一般會員"      
+    elsif accumulated_amount <= 10000
+      "白銀會員"
+    elsif accumulated_amount >= 20000
+      "尊貴會員"  
+    end
+  end
+
+  def still_need(accumulated_amount)
+    if accumulated_amount < 2000
+      2000 - accumulated_amount
+    elsif accumulated_amount < 10000 
+      10000 - accumulated_amount
+    end
   end
 end
