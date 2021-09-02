8776d8f7ebc7fdd7f4dcb6d4a909b49bee220db3
shibao <dream0184@yahoo.com.tw>
2021-09-02 12:23:20 +0800

    WIP: add test page

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,18 @@
 Rails.application.routes.draw do
-
-
-  
+ 
   resources :products
 
-
-
-
-
-
+  resource :cart, only:[:show, :destroy] do
+    collection do
+      post :add, path:'add/:id'
+    end
+  end
 
   get 'hello_world', to: 'hello_world#index'
 
-
-
-
-
-
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }
 
-
+  resources :tests
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
