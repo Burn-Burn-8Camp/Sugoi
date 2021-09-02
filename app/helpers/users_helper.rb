5eac593b2dcdb30d2778abfef1a064b6df358e06
jeter <fl526128@gmail.com>
2021-09-02 15:59:57 +0800

    會員分級

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,27 @@
 module UsersHelper
-  def rank_icon(rank_name)
-    if rank_name == "general"
-        # content_tag()
-      "<i class='far fa-address-book's></i>" 
+  def now_rank_icon(rank_name)
+    if rank_name == "一般會員"
+        # content_tag()  
+      "<i class='far fas fa-award text-6xl text-red-600'></i>"
+     
     else
       "other"
     end      
   end
+
+  def up_rank_icon(rank_name)
+    if rank_name == "一般會員"
+      "<i class='fas fa-medal'></i>"
+    elsif rank_name == "白銀會員"
+      "<i class='fas fa-trophy'></i>"
+    end 
+  end
+
+  def user_rank(rank_name)
+    if rank_name == "一般會員"
+      "白銀會員"
+    elsif rank_name == "白銀會員"
+      "尊貴會員"
+    end
+end
 end
