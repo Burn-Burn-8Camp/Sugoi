e2b2b559479f95fa5cbf239b96d98e0c6b4b4778
jeter <fl526128@gmail.com>
2021-09-04 01:21:38 +0800

    navbar更新

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
     if accumulated_amount <= 2000
         # content_tag()  
       "<i class='far fas fa-award text-6xl text-red-600'></i>"
-    elsif accumulated_amount <= 10000
+    elsif accumulated_amount < 20000
       "<i class='fas fa-medal'></i>"
     elsif accumulated_amount >= 20000
       "<i class='fas fa-trophy'></i>" 
@@ -13,7 +13,7 @@ module UsersHelper
   def up_rank_icon(accumulated_amount)
     if accumulated_amount <= 2000
       "<i class='fas fa-medal'></i>"
-    elsif accumulated_amount <= 10000
+    elsif accumulated_amount < 20000
       "<i class='fas fa-trophy'></i>"
     end 
   end
@@ -21,17 +21,15 @@ module UsersHelper
   def user_rank(accumulated_amount)
     if accumulated_amount <= 2000
       "白銀會員"
-    elsif accumulated_amount <= 10000
+    elsif accumulated_amount < 20000
       "尊貴會員"
-    # elsif accumulated_amount >= 20000
-     
     end
   end
 
-  def up_user_rake(accumulated_amount)
+  def now_user_rank(accumulated_amount)
     if accumulated_amount <= 2000
       "一般會員"      
-    elsif accumulated_amount <= 10000
+    elsif accumulated_amount < 20000
       "白銀會員"
     elsif accumulated_amount >= 20000
       "尊貴會員"  
@@ -41,8 +39,8 @@ module UsersHelper
   def still_need(accumulated_amount)
     if accumulated_amount < 2000
       2000 - accumulated_amount
-    elsif accumulated_amount < 10000 
-      10000 - accumulated_amount
+    elsif accumulated_amount < 20000 
+      20000 - accumulated_amount
     end
   end
 end
