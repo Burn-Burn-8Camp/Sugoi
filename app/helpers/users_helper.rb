b3e20b7526df0df14d4fcb823d2e94c4c75a2ac1
jeter <fl526128@gmail.com>
2021-09-04 23:54:55 +0800

    增加會員分級方法註解

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,6 +9,7 @@ module UsersHelper
       "<i class='fas fa-trophy'></i>" 
     end      
   end
+  #判斷現在的總消費金額之後回傳的icon
 
   def up_rank_icon(accumulated_amount)
     if accumulated_amount <= 2000
@@ -17,6 +18,7 @@ module UsersHelper
       "<i class='fas fa-trophy'></i>"
     end 
   end
+  #判斷現在總消費金額的icon之後顯示下一等級的icon
 
   def user_rank(accumulated_amount)
     if accumulated_amount <= 2000
@@ -25,6 +27,7 @@ module UsersHelper
       "尊貴會員"
     end
   end
+  #顯示下一階段可升級成甚麼會員
 
   def now_user_rank(accumulated_amount)
     if accumulated_amount <= 2000
@@ -35,6 +38,7 @@ module UsersHelper
       "尊貴會員"  
     end
   end
+  #判斷現在的會員等級
 
   def still_need(accumulated_amount)
     if accumulated_amount < 2000
@@ -43,4 +47,5 @@ module UsersHelper
       20000 - accumulated_amount
     end
   end
+  #判斷要完成任務還差多少消費金額
 end
