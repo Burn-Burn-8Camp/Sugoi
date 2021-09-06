2042563a35f295f31452acbba676f0cfdc18ff80
Dennis <dlex351279@gmail.com>
2021-09-06 14:10:44 +0800

    add_reference_to_store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_060153) do
+ActiveRecord::Schema.define(version: 2021_09_06_060505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 2021_09_06_060153) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "total"
     t.string "state"
+    t.bigint "store_id"
+    t.index ["store_id"], name: "index_orders_on_store_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -69,6 +71,8 @@ ActiveRecord::Schema.define(version: 2021_09_06_060153) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
+    t.bigint "store_id"
+    t.index ["store_id"], name: "index_products_on_store_id"
   end
 
   create_table "stores", force: :cascade do |t|
