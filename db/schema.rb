cf04f60f4537066c7cf804b3493c0fa13bac7a23
Alice <f123lori@gmail.com>
2021-09-08 07:04:09 +0800

    add header

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_231354) do
+ActiveRecord::Schema.define(version: 2021_09_07_104303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2021_09_06_231354) do
     t.string "self_web"
     t.string "life_shopping"
     t.string "life_design"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["rank"], name: "index_users_on_rank"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
