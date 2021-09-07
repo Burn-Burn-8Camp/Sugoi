8c0770a694f6e8ef8d7600572ca29505f3eb43bb
jeter <fl526128@gmail.com>
2021-09-07 12:33:15 +0800

    個人名片完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
+    def update
+      @user = User.find(params[:id])
+      
+    end
+
     def profile
     end
 end
