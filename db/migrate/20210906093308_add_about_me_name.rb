fe94490123d836c5549112877c9f2e7c0472c3e0
Alice <f123lori@gmail.com>
2021-09-07 13:59:26 +0800

    修正會員系統

--- db/migrate/20210906093308_add_about_me_name.rb
+++
@@ -1,9 +0,0 @@
-class AddAboutMeName < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :nickname, :string
-    add_column :users, :gender, :string
-    add_column :users, :birth, :date
-    add_column :users, :country, :string
-    add_column :users, :avatar, :st
-  end
-end
--- db/migrate/20210907030217_add_users_self_about.rb
+++
@@ -1,8 +0,0 @@
-class AddUsersSelfAbout < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :self_about, :string
-    add_column :users, :self_blog, :string
-    add_column :users, :nickname, :string
-    add_column :users, :nickname, :string
-    add_column :users, :nickname, :string
-end
