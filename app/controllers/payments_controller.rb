411fc810cb6996eb00dfec3c727a9e9606ea39de
Dennis <dlex351279@gmail.com>
2021-09-11 11:07:51 +0800

    調整刷卡失敗後的對應流程

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,4 @@
 class PaymentsController < ApplicationController
-	before_action  :authenticate_user!
 	skip_before_action :verify_authenticity_token, only: [:notify_response]
 	before_action :find_order, only: [:payment]
 	# 前往付錢
--- app/service/newebpay/mpg.rb
+++ app/service/newebpay/mpg.rb
@@ -30,7 +30,7 @@ module Newebpay
 				info[:TimeStamp] = Time.now.to_i
 				info[:RespondType] = 'JSON'
 				info[:Version] = '1.6'
-				info[:ReturnURL] = 'https://aa19-220-133-155-141.ngrok.io/orders/response'
+				info[:ReturnURL] = 'https://6941-220-133-155-141.ngrok.io/orders/response'
 				info[:NotifyURL] = ''
 				info[:LoginType] = 0
 				info[:CREDIT] = 1
