ce1dbabf37bd16f1d19c107c5ce7dd88858730d7
jeter <fl526128@gmail.com>
2021-09-13 11:01:59 +0800

    賣家Model

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class PaymentsController < ApplicationController
 	skip_before_action :verify_authenticity_token, only: [:notify_response]
 	before_action :find_order, only: [:payment]
@@ -38,3 +39,39 @@ class PaymentsController < ApplicationController
 			@order = current_user.orders.find(params[:id])
 		end
 end
+=======
+class PaymentsController < ApplicationController
+	skip_before_action :verify_authenticity_token, only: [:notify_response]
+	# 前往付錢
+	def payment
+		@order = current_user.orders.find(params[:id])
+		# @form_info = Newebpay::Mpg.new(@order).form_info
+		# @form_data = Newebpay::Mpg.new(@order).info
+	end
+	# 付錢回來
+	def notify_response
+		check_response(return_params(params))
+		
+	end
+
+
+	private
+	def check_response(params)
+		response = Newebpay::MpgResponse.new(params[:TradeInfo])
+		order = Order.find_by(serial: response.order_no)
+		sign_in order.user
+
+		if response.success?
+			order.pay!
+			redirect_to order_path(order), notice: '刷卡成功'
+		else
+			order.cancel!
+			redirect_to order_path(order), notice: '刷卡失敗，訂單取消'
+		end
+	end
+
+	def return_params(params)
+		params.permit(:Status, :MerchantID, :Version, :TradeInfo, :TradeSha)
+	end
+end
+>>>>>>> 賣家Model
