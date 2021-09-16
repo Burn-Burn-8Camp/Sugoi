876e43a07d58de729040001e8e0590c3a75cff41
joanna <a3011662@gmail.com>
2021-09-16 16:44:00 +0800

    解決衝突

--- app/javascript/styles/index.js
+++ app/javascript/styles/index.js
@@ -2,6 +2,7 @@ import "./application.scss"
 import "./pagy_decoration.scss"
 import "./application.scss"
 import "./banner.scss"
+import "./_nav.scss"
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
