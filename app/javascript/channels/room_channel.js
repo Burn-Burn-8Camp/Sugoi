0f74ea0dd823e3339ffc1cc607a8f17457520ce5
Alice <f123lori@gmail.com>
2021-09-19 17:45:26 +0800

    feat: Add Message created datetime

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -12,31 +12,19 @@ document.addEventListener('turbolinks:load', () => {
 
   consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id }, {
     connected() {
-      console.log("connected to " + room_id)
-      // Called when the subscription is ready for use on the server
     },
 
     disconnected() {
-      // Called when the subscription has been terminated by the server
     },
 
     received(data) {
-      console.log(data)
-      // const user_element = document.getElementById('user-id');
-      // const user_id = Number(user_element.getAttribute('data-user-id'));
-
+      const send_btn = document.getElementById('send-btn');
+      const content = document.getElementById('message_content');
       const message_content = document.getElementById('chat-room-content')
-      message_content.innerHTML = message_content.innerHTML + `<p>${data.message} - ${data.user_name}</p>`
-      // let html;
+      message_content.innerHTML = message_content.innerHTML + `<p>${data.message} - ${data.user_name} [ ${data.datetime} ]</p>`
 
-      // if (user_id === data.message.user_id) {
-      //   html = data.mine
-      // } else {
-      //   html = data.theirs
-      // }
-
-      // const messageContainer = document.getElementById('messages')
-      // messageContainer.innerHTML = messageContainer.innerHTML + html
+      send_btn.disabled = false;
+      content.value = ''
     }
   });
 })
