2373b76ab4c26548d427698ed0ec34d416a9190d
Dennis <dlex351279@gmail.com>
2021-09-23 10:59:13 +0800

    引入輪播圖套件與樣式，移除stimulus js

--- app/javascript/components/react_application.js
+++ app/javascript/components/react_application.js
@@ -3,10 +3,12 @@ import BuyerCommentForm from './buyer_comment_form'
 import OrderItemsInfo from './order_items_info'
 import StarList from './star_list'
 import SellerCommentForm from './seller_comment_form'
+import BannerSlider from './banner_slider'
 
 ReactOnRails.register({
 	BuyerCommentForm, 
 	OrderItemsInfo, 
 	StarList,
-	SellerCommentForm
+	SellerCommentForm,
+	BannerSlider
 });
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,11 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "chartkick/chart.js"
 import "channels"
 import "styles"
 import "components"
 import "scripts"
+import "chartkick/chart.js"
 
 Rails.start()
 Turbolinks.start()
