653ee9ad4b9725c02f59cc729edcb03046a97500
jeter <fl526128@gmail.com>
2021-09-23 16:03:43 +0800

    賣家申請UI完成

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,57 +1,66 @@
 class StoresController < ApplicationController
-	before_action  :authenticate_user!
+	before_action :authenticate_user!
+	before_action :require_seller!
 	before_action :find_store, except: [:show, :new]
 
 	def show
 	end
 
 	def new
-		@store = Store.new
+	  @store = Store.new
 	end
 
 	def create
-		@store = Store.new(store_params)
-		if @store.save
-			redirect_to store_path, notice: '商店創建成功'
-		else
-			render :new, notice: '請重新填寫'
-		end
+	  @store = Store.new(store_params)
+	  if @store.save
+	  redirect_to store_path, notice: '商店創建成功'
+	  else
+	  render :new, notice: '請重新填寫'
+	  end
 	end
 
 	def edit
+	  @store = current_store
 	end
 
 	def update
-		if @store.update(store_params)
-			redirect_to store_path, notice: '更新成功'
-		else
-			render :edit, notice: '請重新填寫'
-		end
+	  if @store.update(store_params)
+	    redirect_to store_path, notice: "修改成功"
+	  else
+		render :edit
+	  end
 	end
 	
 	def products_list
-		@products = current_store.products.all
+	  @products = current_store.products.all
 	end
 
 	def product_detail
-		@product = current_store.products.find(params[:id])
+	  @product = current_store.products.find(params[:id])
 	end
 
 	def orders_list
-		@orders = current_store.orders.all
+	  @orders = current_store.orders.all
 	end
 
 	def order_detail
-		@order = current_store.orders.find_by_friendly_id!(params[:id])
-		@items = @order.order_items.includes(:store).where(store: current_store)
+	  @order = current_store.orders.find(params[:id])
+	  @items = @order.order_items.includes(:product)
 	end
-	
+
 	private
-		def store_params
-			params.require(:store).permit(:name, :user_id, :introduction)
-		end
+	  def store_params
+		params.require(:store).permit(:name, :user_id, :introduction)
+	  end
 		
-		def find_store
-			@store = current_user.store
+	  def find_store
+		@store = current_user.store 
+	  end
+
+	  def require_seller!
+		if current_user.role != "seller"
+		  flash[:alert] = "您的權限不足"
+		  redirect_to root_path
 		end
+	  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   mount_uploader :image, ImageUploader
   
-  validates_uniqueness_of :name
+  # validates_uniqueness_of :name
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2 ,:github]
