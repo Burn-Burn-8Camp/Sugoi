23b383f14e503c6a453bf8a64bd4ebebd67ea38d
jeter <fl526128@gmail.com>
2021-09-24 18:24:43 +0800

    Store_UI_finsh

--- app/javascript/components/react_application.js
+++ app/javascript/components/react_application.js
@@ -4,12 +4,13 @@ import OrderItemsInfo from './order_items_info'
 import StarList from './star_list'
 import SellerCommentForm from './seller_comment_form'
 import BannerSlider from './banner_slider'
-
-
+import StorePage from './store_page'
 ReactOnRails.register({
 	BuyerCommentForm, 
 	OrderItemsInfo, 
 	StarList,
 	SellerCommentForm,
-	BannerSlider
+	BannerSlider,
+	StorePage,
+	PopUpScreen,
 });
