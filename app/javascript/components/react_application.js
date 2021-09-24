4057b09497a787d7b29b890d59e58bf10306e2e7
jeter <fl526128@gmail.com>
2021-09-24 18:24:43 +0800

    Store_UI_finsh

--- app/javascript/components/react_application.js
+++ app/javascript/components/react_application.js
@@ -1,7 +1,11 @@
 import ReactOnRails from 'react-on-rails';
 import PopUpScreen from './pop_up_screen'
 import OrderItemsInfo from './order_items_info'
-
+import StorePage from './store_page'
+import Verify from './verify'
 ReactOnRails.register({
-	PopUpScreen, OrderItemsInfo
+	PopUpScreen,
+	StorePage,
+	OrderItemsInfo,
+	Verify,
 });
--- app/javascript/scripts/index.js
+++ app/javascript/scripts/index.js
@@ -1,9 +1,3 @@
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
-const application = Application.start()
-const context = require.context(".", true, /\.js$/)
-application.load(definitionsFromContext(context))
 import "./confirmCart"
 import "./swiftImg"
 import '@fortawesome/fontawesome-free/js/all.js'
