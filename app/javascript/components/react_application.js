451103d54c98352e4da5e675cd8ae5ef21e553d2
jeter <fl526128@gmail.com>
2021-09-25 19:08:26 +0800

    整理縮排

--- app/javascript/components/react_application.js
+++ app/javascript/components/react_application.js
@@ -2,7 +2,6 @@ import ReactOnRails from 'react-on-rails';
 import PopUpScreen from './pop_up_screen'
 import OrderItemsInfo from './order_items_info'
 import StorePage from './store_page'
-import Verify from './verify'
 ReactOnRails.register({
 	PopUpScreen,
 	StorePage,
